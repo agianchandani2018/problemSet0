7cdbda4e04c69194734b76fccbb232bd8ff06254
Ami Gianchandani <agianchandani2018@pingry.org>
2016-03-29 21:54:36 -0400

    Added files via upload
    Initial commit. Not all tests working

---
+++ ps0-runner.py
@@ -0,0 +1,54 @@
+import ps0
+
+#0 - even or odd
+#DONE
+print("The number 7 is even: {} ".format(ps0.even_or_odd(7)))
+print("The number 8 is even: {} ".format(ps0.even_or_odd(8)))
+print("The number 0 is even: {} ".format(ps0.even_or_odd(0)))
+
+#1 - number of digits
+#TODO
+# print("3 has {} digits".format(ps0.number_digits(3)))
+# print("34 has {} digits".format(ps0.number_digits(34)))
+# print("345 has {} digits".format(ps0.number_digits(345)))
+
+#2 - sum of digits
+#DONE
+print("The sum of the digits of 1 is {}".format(ps0.sum_digits(1)))
+print("The sum of the digits of 12 is {}".format(ps0.sum_digits(12)))
+print("The sum of the digits of 123 is {}".format(ps0.sum_digits(123)))
+print("The sum of the digits of 1239 is {}".format(ps0.sum_digits(1239)))
+
+
+#3 - sum of preceeding
+#TODO
+
+
+
+#4 - factorial
+#DONE
+print("The factorial of 5 is {}".format(ps0.factorial(5)))
+print("The factorial of 4 is {}".format(ps0.factorial(4)))
+
+
+
+#5 - factor
+#TODO
+# print("3 is a factor of 18 is {}".format(ps0.find_factor(18)))
+
+#6 - prime
+#halfway there
+
+print("9 is prime: {}".format(ps0.is_prime(6)))
+print("13 is prime: {}".format(ps0.is_prime(13)))
+
+#7 - perfect
+#TODO
+
+print("6 is perfect: {}".format(ps0.is_perfect(6)))
+
+
+#8 - sum divisible
+#TODO
+
+
